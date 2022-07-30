1ee929ec743e8a0b61dbfebf651152549293b87a
Prateek Pai <paiprateek123@gmail.com>
2022-07-30 22:27:49 +0530

    Program to Find Harmonic number upto a range

--- RepetitionPracticeProblemsWithLoop.js
+++ RepetitionPracticeProblemsWithLoop.js
@@ -1,9 +1,9 @@
-console.log("Welcome to Repetition Practice Problems with loops")
+var range=prompt("Enter range to find Harmonic number: ");
 
-var range=prompt("Enter range to find power of two: ");
-var sum=1
-for (var i=1; i<=range; i++){
-    sum=sum*2;
+var sum=0;
+
+for (var i=1;i<=range;i++){
+    sum=sum+(1/i);
 }
 
-console.log("Power of two table upto the range is: ",sum);
+console.log("Harmonic number upto that range is: ",sum);
